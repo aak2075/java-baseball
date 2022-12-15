36e4aa656a4a1a7a87427aa15c8691cb6b9b00f6
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-12-15 23:19:40 +0900

    refactor(baseball) : exception 의 종류를 IllegalArgumentException 으로 변경

--- src/main/java/baseball/exceptions/NumberRangeException.java
+++ src/main/java/baseball/exceptions/NumberRangeException.java
@@ -4,6 +4,6 @@ public class NumberRangeException extends RuntimeException {
     private static final String NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 범위에 해당하지 않는 숫자 입력";
 
     public NumberRangeException() {
-        super(NUMBER_RANGE_ERROR_MESSAGE);
+        throw new IllegalArgumentException(NUMBER_RANGE_ERROR_MESSAGE);
     }
 }
--- src/main/java/baseball/exceptions/NumberSizeException.java
+++ src/main/java/baseball/exceptions/NumberSizeException.java
@@ -4,6 +4,6 @@ public class NumberSizeException extends RuntimeException {
     private static final String NUMBER_SIZE_ERROR_MESSAGE = "[ERROR] 입력의 크기는 3 입니다.";
 
     public NumberSizeException() {
-        super(NUMBER_SIZE_ERROR_MESSAGE);
+        throw new IllegalArgumentException(NUMBER_SIZE_ERROR_MESSAGE);
     }
 }
