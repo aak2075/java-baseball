6ca82665e49b75dc769ff530db96bea8886af9ab
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-12-15 19:48:10 +0900

    chore(baseball) : GameNumberTest 패키지 이동

--- src/test/java/baseball/domain/GameNumberTest.java
+++ src/test/java/baseball/domain/GameNumberTest.java
@@ -1,6 +1,5 @@
-package baseball;
+package baseball.domain;
 
-import baseball.domain.GameNumber;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
