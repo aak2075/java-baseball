87051451454400e40e4f3a9bde9a8dfb082b13e6
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-12-15 23:22:58 +0900

    test(baseball) : 예외 사항에 대한 테스트 추가

--- src/test/java/baseball/domain/GameNumberTest.java
+++ src/test/java/baseball/domain/GameNumberTest.java
@@ -2,11 +2,13 @@ package baseball.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class GameNumberTest {
     GameNumber answer;
@@ -34,5 +36,29 @@ public class GameNumberTest {
         assertThat(answer.containsOnly(input, 1)).isFalse();
         assertThat(answer.containsOnly(input, 2)).isFalse();
         assertThat(answer.containsOnly(input, 3)).isTrue();
+    }
+
+    @Nested
+    class expected {
+        @DisplayName("숫자의 사이즈가 3이 아닌 경우")
+        @Test
+        void size() {
+            assertThatThrownBy(() -> new GameNumber(List.of(1,2,3,4)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("숫자의 범위가 1~9가 아닌 경우")
+        @Test
+        void range() {
+            assertThatThrownBy(() -> new GameNumber(List.of(0,1,2)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("숫자에 중복이 포함된 경우")
+        @Test
+        void duplicate() {
+            assertThatThrownBy(() -> new GameNumber(List.of(1,2,2)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
